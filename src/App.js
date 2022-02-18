e6a8cdb3ef8668c824d80080c9bbc664a43d20a0
sohanr <mizan.rahman66d@gmail.com>
2022-02-18 23:16:21 +0600

    setPopular

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
 
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() =>{
+    fetchPopular()
+  },[])
+
 
   const fetchPopular = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=4d924c861f0ad983bad92ce12a9610ff&language=en-US&page=1')
 
-    const movies = awat data.json();
+    const movies = await data.json();
 
+    console.log(movies);
+    setPopular(movies.results);
 
   }
 
@@ -16,7 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{process.env.NAME}</h1>
+      <h1>hello</h1>
     </div>
   );
 }
