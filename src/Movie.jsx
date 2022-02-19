1b54f941a7414902156ec08141f202d2408323f9
sohanr <mizan.rahman66d@gmail.com>
2022-02-19 18:28:59 +0600

    movie component

---
+++ src/Movie.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Movie = ({movie}) => {
+  return (
+    <div>
+        <h2>{movie.title}</h2>
+        <img src={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path} alt={movie.title + "image"}  />
+    </div>
+  )
+}
+
+export default Movie;
